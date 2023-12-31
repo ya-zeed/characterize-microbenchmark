16a9620788e031e42599c6ef1ec8e65c1707db8c
Yazeed <yazoid1421@gmail.com>
2023-12-31 23:44:25 +0300

    SIMD

--- src/blackscholes/impl/scalar.c
+++ src/blackscholes/impl/scalar.c
@@ -8,16 +8,169 @@
 
 /* Standard C includes */
 #include <stdlib.h>
+#include <printf.h>
 
 /* Include common headers */
 #include "common/macros.h"
 #include "common/types.h"
+#include "math.h"
 
 /* Include application-specific headers */
 #include "include/types.h"
 
 /* Naive Implementation */
-void* impl_scalar(void* args)
-{
-  return NULL;
+
+#define inv_sqrt_2xPI 0.39894228040143270286
+
+float CNDF_Scalar(float InputX) {
+    int sign;
+
+    float OutputX;
+    float xInput;
+    float xNPrimeofX;
+    float expValues;
+    float xK2;
+    float xK2_2, xK2_3;
+    float xK2_4, xK2_5;
+    float xLocal, xLocal_1;
+    float xLocal_2, xLocal_3;
+
+    // Check for negative value of InputX
+    if (InputX < 0.0) {
+        InputX = -InputX;
+        sign = 1;
+    } else
+        sign = 0;
+
+    xInput = InputX;
+
+    // Compute NPrimeX term common to both four & six decimal accuracy calcs
+    expValues = exp(-0.5f * InputX * InputX);
+    xNPrimeofX = expValues;
+    xNPrimeofX = xNPrimeofX * inv_sqrt_2xPI;
+
+    xK2 = 0.2316419 * xInput;
+    xK2 = 1.0 + xK2;
+    xK2 = 1.0 / xK2;
+    xK2_2 = xK2 * xK2;
+    xK2_3 = xK2_2 * xK2;
+    xK2_4 = xK2_3 * xK2;
+    xK2_5 = xK2_4 * xK2;
+
+    xLocal_1 = xK2 * 0.319381530;
+    xLocal_2 = xK2_2 * (-0.356563782);
+    xLocal_3 = xK2_3 * 1.781477937;
+    xLocal_2 = xLocal_2 + xLocal_3;
+    xLocal_3 = xK2_4 * (-1.821255978);
+    xLocal_2 = xLocal_2 + xLocal_3;
+    xLocal_3 = xK2_5 * 1.330274429;
+    xLocal_2 = xLocal_2 + xLocal_3;
+
+    xLocal_1 = xLocal_2 + xLocal_1;
+    xLocal = xLocal_1 * xNPrimeofX;
+    xLocal = 1.0 - xLocal;
+
+    OutputX = xLocal;
+
+    if (sign) {
+        OutputX = 1.0 - OutputX;
+    }
+
+    return OutputX;
+}
+
+
+float blackScholesScalar(float sptprice, float strike, float rate, float volatility,
+                         float otime, int otype, float timet) {
+    float OptionPrice;
+
+    // local private working variables for the calculation
+    float xStockPrice;
+    float xStrikePrice;
+    float xRiskFreeRate;
+    float xVolatility;
+    float xTime;
+    float xSqrtTime;
+
+    float logValues;
+    float xLogTerm;
+    float xD1;
+    float xD2;
+    float xPowerTerm;
+    float xDen;
+    float d1;
+    float d2;
+    float FutureValueX;
+    float NofXd1;
+    float NofXd2;
+    float NegNofXd1;
+    float NegNofXd2;
+
+    xStockPrice = sptprice;
+    xStrikePrice = strike;
+    xRiskFreeRate = rate;
+    xVolatility = volatility;
+
+    xTime = otime;
+    xSqrtTime = sqrt(xTime);
+
+    logValues = log(sptprice / strike);
+
+    xLogTerm = logValues;
+
+
+    xPowerTerm = xVolatility * xVolatility;
+    xPowerTerm = xPowerTerm * 0.5;
+
+    xD1 = xRiskFreeRate + xPowerTerm;
+    xD1 = xD1 * xTime;
+    xD1 = xD1 + xLogTerm;
+
+    xDen = xVolatility * xSqrtTime;
+    xD1 = xD1 / xDen;
+    xD2 = xD1 - xDen;
+
+    d1 = xD1;
+    d2 = xD2;
+
+    NofXd1 = CNDF_Scalar(d1);
+    NofXd2 = CNDF_Scalar(d2);
+
+    FutureValueX = strike * (exp(-(rate) * (otime)));
+    if (otype == 0) {
+        OptionPrice = (sptprice * NofXd1) - (FutureValueX * NofXd2);
+    } else {
+        NegNofXd1 = (1.0 - NofXd1);
+        NegNofXd2 = (1.0 - NofXd2);
+        OptionPrice = (FutureValueX * NegNofXd2) - (sptprice * NegNofXd1);
+    }
+
+    return OptionPrice;
+}
+
+void blackScholesHelperScalar(float *sptprice, float *strike, float *rate,
+                              float *volatility, float *otime, char *otype,
+                              float *OptionPrice, size_t size) {
+    for (int i = 0; i < size; i++) {
+        char type = otype[i] == 'C' ? 0 : 1;
+        OptionPrice[i] = blackScholesScalar(sptprice[i], strike[i], rate[i],
+                                            volatility[i], otime[i], type,
+                                            0);
+    }
+
+}
+
+void *impl_scalar(void *args) {
+    args_t *parsed_args = (args_t *) args;
+
+    blackScholesHelperScalar(
+            (float *) parsed_args->sptPrice,
+            (float *) parsed_args->strike,
+            (float *) parsed_args->rate,
+            (float *) parsed_args->volatility,
+            (float *) parsed_args->otime,
+            (char *) parsed_args->otype,
+            (float *) parsed_args->output,
+            parsed_args->num_stocks
+    );
 }
--- src/blackscholes/impl/vec.c
+++ src/blackscholes/impl/vec.c
@@ -8,16 +8,176 @@
 
 /* Standard C includes */
 #include <stdlib.h>
+#include <printf.h>
+#include "arm_neon.h"
+
 
 /* Include common headers */
 #include "common/macros.h"
 #include "common/types.h"
+#include "common/vmath.h"
+#include "common/neon_mathfun.h"
 
 /* Include application-specific headers */
 #include "include/types.h"
 
-/* Alternative Implementation */
-void* impl_vector(void* args)
-{
-  return NULL;
+#define inv_sqrt_2xPI 0.39894228040143270286
+
+float32x4_t CNDF(float32x4_t InputX) {
+    const float32x4_t vInvSqrt2xPI = vdupq_n_f32(inv_sqrt_2xPI);
+
+    float32x4_t vSign = vcltq_f32(InputX, vdupq_n_f32(0.0));
+    float32x4_t vInputX = vabsq_f32(InputX);
+
+    float32x4_t xK2, xK2_2, xK2_3, xK2_4, xK2_5;
+    float32x4_t xLocal, xLocal_1, xLocal_2, xLocal_3;
+
+    float32x4_t vNPrimeofX = exp_ps(
+            vmulq_f32(
+                    vdupq_n_f32(-0.5),
+                    vmulq_f32(vInputX, vInputX)
+            )
+    );
+    vNPrimeofX = vmulq_f32(vNPrimeofX, vInvSqrt2xPI);
+
+    xK2 = vmulq_f32(vdupq_n_f32(0.2316419), vInputX);
+    xK2 = vaddq_f32(vdupq_n_f32(1.0), xK2);
+    xK2 = vdivq_f32(vdupq_n_f32(1.0), xK2);
+    xK2_2 = vmulq_f32(xK2, xK2);
+    xK2_3 = vmulq_f32(xK2_2, xK2);
+    xK2_4 = vmulq_f32(xK2_3, xK2);
+    xK2_5 = vmulq_f32(xK2_4, xK2);
+
+    xLocal_1 = vmulq_f32(xK2, vdupq_n_f32(0.319381530));
+    xLocal_2 = vmulq_f32(xK2_2, vdupq_n_f32(-0.356563782));
+    xLocal_3 = vmulq_f32(xK2_3, vdupq_n_f32(1.781477937));
+    xLocal_2 = vaddq_f32(xLocal_2, xLocal_3);
+    xLocal_3 = vmulq_f32(xK2_4, vdupq_n_f32(-1.821255978));
+    xLocal_2 = vaddq_f32(xLocal_2, xLocal_3);
+    xLocal_3 = vmulq_f32(xK2_5, vdupq_n_f32(1.330274429));
+    xLocal_2 = vaddq_f32(xLocal_2, xLocal_3);
+
+    xLocal_1 = vaddq_f32(xLocal_2, xLocal_1);
+    xLocal = vmulq_f32(xLocal_1, vNPrimeofX);
+    xLocal = vsubq_f32(vdupq_n_f32(1.0), xLocal);
+
+    float32x4_t signedOutput = vsubq_f32(vdupq_n_f32(1.0), xLocal);
+
+    return vbslq_f32(vSign, signedOutput, xLocal);;
 }
+
+
+float32x4_t
+blackScholes(float32x4_t vSptPrice, float32x4_t vStrike,
+             float32x4_t vRate, float32x4_t vVolatility,
+             float32x4_t vOtime, uint32x4_t vOType) {
+
+    float32x4_t vSqrtTime = vsqrtq_f32(vOtime);
+    float32x4_t vLogTerm = log_ps(
+            vdivq_f32(vSptPrice, vStrike)
+    );
+
+    float32x4_t vPowerTerm = vmulq_f32(vVolatility, vVolatility);
+    vPowerTerm = vmulq_f32(vPowerTerm, vdupq_n_f32(0.5));
+
+    float32x4_t xD1 = vaddq_f32(vRate, vPowerTerm);
+    xD1 = vmulq_f32(xD1, vOtime);
+    xD1 = vaddq_f32(xD1, vLogTerm);
+
+    float32x4_t vDen = vmulq_f32(vVolatility, vSqrtTime);
+    xD1 = vdivq_f32(xD1, vDen);
+    float32x4_t xD2 = vsubq_f32(xD1, vDen);
+
+    float32x4_t vCDFd1 = CNDF(xD1);
+    float32x4_t vCDFd2 = CNDF(xD2);
+
+    float32x4_t vFutureValueX = vmulq_f32(
+            vStrike,
+            exp_ps(
+                    vmulq_f32(
+                            vmulq_f32(vdupq_n_f32(-1), vRate),
+                            vOtime
+                    )
+            )
+    );
+
+    float32x4_t vOptionPriceOTypeC = vsubq_f32(
+            vmulq_f32(vSptPrice, vCDFd1),
+            vmulq_f32(vFutureValueX, vCDFd2)
+    );
+
+    float32x4_t vOptionPriceOTypeP = vsubq_f32(
+            vmulq_f32(vFutureValueX, vsubq_f32(vdupq_n_f32(1), vCDFd2)),
+            vmulq_f32(vSptPrice, vsubq_f32(vdupq_n_f32(1), vCDFd1))
+    );
+
+    int32x4_t v67 = vdupq_n_s32(67);
+
+    float32x4_t vOptionPrice = vbslq_f32(
+            vceqq_s32(vOType, v67),
+            vOptionPriceOTypeC,
+            vOptionPriceOTypeP
+    );
+
+    return vOptionPrice;
+}
+
+void blackScholesHelper(float *sptprice, float *strike, float *rate,
+                        float *volatility, float *otime, char *otype,
+                        float *output, size_t numOptions) {
+
+    int i;
+    float32x4_t vSptPrice, vStrike, vRate, vVolatility, vOtime;
+    int32x4_t vOType;
+
+    for (i = 0; i < numOptions; i += 4) {
+        int remainingOptions = numOptions - i;
+        if (remainingOptions > 4) remainingOptions = 4;
+
+        // Load data
+        vSptPrice = vld1q_f32(sptprice + i);
+        vStrike = vld1q_f32(strike + i);
+        vRate = vld1q_f32(rate + i);
+        vVolatility = vld1q_f32(volatility + i);
+        vOtime = vld1q_f32(otime + i);
+
+        uint8x16_t vOTypeChar = vld1q_u8(otype + i);
+        uint16x8_t vOTypeHalf = vmovl_u8(vget_low_u8(vOTypeChar));
+        vOType = vmovl_u16(vget_low_u16(vOTypeHalf));
+
+        // Compute option prices
+        float32x4_t vOptionPrices = blackScholes(vSptPrice, vStrike, vRate, vVolatility, vOtime, vOType);
+
+        // Temporarily store the results in an array
+        float temp[4];
+        vst1q_f32(temp, vOptionPrices);
+
+        // Copy the needed results to the output
+        for (int j = 0; j < remainingOptions; j++) {
+            output[i + j] = temp[j];
+        }
+
+
+    }
+
+}
+
+/* Alternative Implementation */
+void *
+impl_vector(void *args) {
+    /* Get the argument struct */
+    args_t *parsed_args = (args_t *) args;
+
+    blackScholesHelper(
+            parsed_args->sptPrice,
+            parsed_args->strike,
+            parsed_args->rate,
+            parsed_args->volatility,
+            parsed_args->otime,
+            parsed_args->otype,
+            parsed_args->output,
+            parsed_args->num_stocks
+    );
+}
+
+
